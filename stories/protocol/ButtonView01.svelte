6e4dc5d92fefebde0aea7a600c4b6fb3d674f830
hamilton <hamilton.ulmer@gmail.com>
2020-10-19 13:38:29 -0700

    adds protocol; implements protocol story for button

---
+++ stories/protocol/ButtonView01.svelte
@@ -0,0 +1,42 @@
+<script>
+  import Button from "../../src/components/Button.svelte";
+</script>
+
+<style>
+  .container {
+    display: grid;
+    grid-template-columns: max-content max-content;
+    grid-gap: 2rem;
+    flex-wrap: wrap;
+  }
+
+  section {
+    display: grid;
+    grid-template-columns: max-content max-content;
+    grid-gap: 1rem;
+  }
+
+  div {
+    text-align: center;
+  }
+</style>
+
+<div class="container">
+  {#each ['sm', 'md', 'lg', 'xl'] as size}
+    <section>
+      <div style="grid-column: 1/3; font-weight: bold;">Size: {size}</div>
+      <div>Active</div>
+      <div>Disabled</div>
+      <Button {size}>standard</Button>
+      <Button {size} disabled>standard</Button>
+      <Button {size} secondary>secondary</Button>
+      <Button {size} secondary disabled>secondary</Button>
+      <Button {size} product>product</Button>
+      <Button {size} product disabled>product</Button>
+      <Button {size} product secondary>product / secondary</Button>
+      <Button {size} product secondary disabled>product / secondary</Button>
+      <Button {size} neutral>neutral</Button>
+      <Button {size} neutral disabled>neutral</Button>
+    </section>
+  {/each}
+</div>
