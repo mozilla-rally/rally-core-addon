e55f0fda33dab0788d42ed6d64a493a9694a2cae
Alessio Placitelli <alessio.placitelli@gmail.com>
2020-10-20 11:19:34 +0200

    Catch the 'state-update' message in the front-end

--- src/create-store.js
+++ src/create-store.js
@@ -1,33 +1,24 @@
 import { writable } from "svelte/store";
 import { produce } from "immer/dist/immer.cjs.production.min.js";
 
-const STORE_KEY = "__STATE__";
 export default function createStore(initialState, api) {
   // initialize the writable store.
   const { subscribe, update, set } = writable();
 
   // initialize from the API.
-  api.initialize(STORE_KEY, initialState).then(async (remoteInitialState) => {
+  api.initialize(initialState).then(async (remoteInitialState) => {
+    console.log('initialize', remoteInitialState, initialState);
     const state = remoteInitialState || initialState;
     state.availableStudies = await api.getAvailableStudies();
     set(state);
   });
 
+  // set UI when the background script reports a new state.
+  api.onNextState(set);
+
   return {
     subscribe,
     set,
-    produce(fcn) {
-      update((draft) => {
-        const nextState = produce(draft, fcn);
-        api.setItem(STORE_KEY, nextState);
-        return nextState;
-      });
-    },
-    setField(key, value) {
-      this.produce((state) => {
-        state[key] = value;
-      });
-    },
     async updateStudyEnrollment(studyID, enroll) {
       // Enforce the truthyness of `enroll`, to make sure
       // it's always a boolean.
@@ -35,24 +26,11 @@ export default function createStore(initialState, api) {
       console.debug(
         `Ion - changing study ${studyID} enrollment to ${coercedEnroll}`);
 
-      let outcome;
       // send study enrollment message
       try {
-        outcome = await api.updateStudyEnrollment(studyID, coercedEnroll);
+        await api.updateStudyEnrollment(studyID, coercedEnroll);
       } catch (err) {
         console.error(err);
-      }
-      // if study enrollment is successful, update frontend.
-      if (outcome) {
-        this.produce((draft) => {
-          if (draft.activeStudies.includes(studyID)) {
-            draft.activeStudies = draft.activeStudies.filter(
-              (id) => id !== studyID
-            );
-          } else {
-            draft.activeStudies.push(studyID);
-          }
-        });
       }
     },
     async updateIonEnrollment(enroll) {
@@ -61,18 +39,11 @@ export default function createStore(initialState, api) {
       let coercedEnroll = !!enroll;
       console.debug(`Ion - changing enrollment to ${coercedEnroll}`);
 
-      let outcome;
       // send the ion enrollment message
       try {
-        outcome = await api.updateIonEnrollment(coercedEnroll);
+        await api.updateIonEnrollment(coercedEnroll);
       } catch (err) {
         console.error(err);
-      }
-      // if ion enrollment is successful, update frontend.
-      if (outcome) {
-        this.produce((draft) => {
-          draft.enrolled = coercedEnroll;
-        });
       }
     },
   };
