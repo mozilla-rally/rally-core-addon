43da9e64a31b42291256f3681642e30ade34feac
Hamilton Ulmer <hamilton.ulmer@gmail.com>
2020-10-23 09:53:01 -0700

    adds @mozilla-protocol/protocol (#95)
    * adds protocol; implements protocol story for button
    
    * adds in missing protocol stylesheet for storybook

---
+++ src/components/Button.svelte
@@ -0,0 +1,31 @@
+<script>
+  import { get_current_component as getComponent } from "svelte/internal";
+  import { forwardEvents } from "./forwardEvents";
+  export let size = "md";
+  export let neutral = false;
+  export let product = false;
+  export let secondary = false;
+  export let dark = false;
+
+  export let disabled = false;
+
+  const forwardAll = forwardEvents(getComponent());
+
+  $: sizeClass = size ? `mzp-t-${size}` : undefined;
+  $: productClass = product ? `mzp-t-product` : undefined;
+  $: neutralClass = neutral ? `mzp-t-neutral` : undefined;
+  $: secondaryClass = secondary ? `mzp-t-secondary` : undefined;
+  $: darkClass = dark ? `mzp-t-dark` : undefined;
+  $: classSet = [
+    "mzp-c-button",
+    sizeClass,
+    productClass,
+    neutralClass,
+    darkClass,
+    secondaryClass,
+  ]
+    .filter((t) => t)
+    .join(" ");
+</script>
+
+<button use:forwardAll class={classSet} {disabled}><slot /></button>
---
+++ stories/protocol/button.stories.js
@@ -0,0 +1,9 @@
+import ButtonView01 from './ButtonView01.svelte';
+
+export default {
+    title: "Button.svelte",
+  };
+  
+  export const Basic = () => ({
+    Component: ButtonView01,
+  });
