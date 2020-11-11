82d3f9fe9bef7f1365dca19dabfd46e919a51f21
hamilton <hamilton.ulmer@gmail.com>
2020-11-11 07:34:15 -0800

    fixes case issue from rebase

--- src/routes/Sidebar.svelte
+++ src/routes/Sidebar.svelte
@@ -2,7 +2,7 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-  import MainSidebar from "../components/Layouts/MainSidebar.svelte";
+  import MainSidebar from "../components/layouts/MainSidebar.svelte";
   import ExternalLink from "../components/icons/ExternalLink.svelte";
 </script>
 
