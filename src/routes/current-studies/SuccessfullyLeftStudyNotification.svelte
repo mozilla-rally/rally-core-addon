31c2bf4436ce8f1cfa4811464c7d55a9b907ec5a
hamilton <hamilton.ulmer@gmail.com>
2020-11-19 07:53:33 -0800

    fixes two more newline issues

--- src/routes/current-studies/SuccessfullyLeftStudyNotification.svelte
+++ src/routes/current-studies/SuccessfullyLeftStudyNotification.svelte
@@ -1,18 +1,18 @@
 <script>
-    /* This Source Code Form is subject to the terms of the Mozilla Public
-     * License, v. 2.0. If a copy of the MPL was not distributed with this
-     * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-    
-    import Notification from '../../components/notification/NotificationElement.svelte';
-    import Check from '../../components/icons/Check.svelte';
-    
-    export let xOffset;
-    export let location = "top";
+/* This Source Code Form is subject to the terms of the Mozilla Public
+    * License, v. 2.0. If a copy of the MPL was not distributed with this
+    * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-    </script>
-    <Notification {location} {xOffset}>
-        <span style="display: contents;" slot="icon">
-        <Check />
-        </span>
-        <span slot="body">successfully left study</span>
-    </Notification>
+import Notification from '../../components/notification/NotificationElement.svelte';
+import Check from '../../components/icons/Check.svelte';
+
+export let xOffset;
+export let location = "top";
+
+</script>
+<Notification {location} {xOffset}>
+    <span style="display: contents;" slot="icon">
+    <Check />
+    </span>
+    <span slot="body">successfully left study</span>
+</Notification>
