b81b6d4aa0f52f1823709893d73055324bbac05c
Alessio Placitelli <alessio.placitelli@gmail.com>
2020-12-17 11:04:06 +0100

    Fix the test fallout due to the Ion renaming

--- tests/core-addon/DataCollection.test.js
+++ tests/core-addon/DataCollection.test.js
@@ -74,7 +74,7 @@ describe('DataCollection', function () {
     it('rejects if no study id is provided', function () {
       assert.rejects(
         this.dataCollection.sendDeletionPing("some-rally-id"),
-        { message: "IonCore - the deletion-request ping requires a study id"}
+        { message: "DataCollection - the deletion-request ping requires a study id"}
       );
     });
 
