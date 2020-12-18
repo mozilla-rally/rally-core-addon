68142b0c468b0e38d372ae18a5266e532d80f357
Alessio Placitelli <alessio.placitelli@gmail.com>
2020-12-18 11:11:37 +0100

    Rename the core addon in the partner support library

--- support/rally.js
+++ support/rally.js
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-const CORE_ADDON_ID = "ion-core-addon@mozilla.org";
+const CORE_ADDON_ID = "rally-core@mozilla.org";
 const SIGNUP_URL = "https://mozilla-rally.github.io/core-addon/";
 
 class Rally {
