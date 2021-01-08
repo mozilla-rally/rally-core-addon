4bc3b0a59d17865d8eeea8cd511162444052b3c5
Alessio Placitelli <alessio.placitelli@gmail.com>
2021-01-08 10:42:42 +0100

    Remove unused legacy code (dispatchFxEvent)

--- src/stores.js
+++ src/stores.js
@@ -6,17 +6,5 @@ import createStore from "./create-store";
 import api from "./api/__API_ENDPOINT__";
 
 const initialValue = __STORE_IMPLEMENTATION__;
-const webChannelId = "pioneer";
-
-export const dispatchFxEvent = (message) => {
-  window.dispatchEvent(
-    new window.CustomEvent("WebChannelMessageToChrome", {
-      detail: JSON.stringify({
-        id: webChannelId,
-        message: message,
-      }),
-    })
-  );
-};
 
 export const store = createStore(initialValue, api);
