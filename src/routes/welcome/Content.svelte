3f3fb6cf3933600ae796db2557c2dbdeac980399
hamilton <hamilton.ulmer@gmail.com>
2021-01-27 10:33:57 -0800

    updates copy of notice; widens content body to 700px

--- src/routes/welcome/Content.svelte
+++ src/routes/welcome/Content.svelte
@@ -6,9 +6,6 @@
   import { fly } from "svelte/transition";
   import { createEventDispatcher } from "svelte";
   import Button from "../../components/Button.svelte";
-  import Graph from "./Graph.svelte";
-  import Profile from "./Profile.svelte";
-  import Study from "./Study.svelte";
   import Logo from "../../components/MozillaRallyLogo.svelte";
   import ExternalLink from "../../components/icons/ExternalLink.svelte";
   const dispatch = createEventDispatcher();
@@ -41,7 +38,7 @@
     grid-auto-flow: column;
     align-items: center;
     justify-content: center;
-    grid-column-gap: 0.25rem;
+    grid-column-gap: 10.67px;
   }
 
   .ion-three-cards {
@@ -49,28 +46,20 @@
     grid-template-columns: auto auto auto;
     justify-content: space-evenly;
     justify-items: center;
-    padding-top: 60px;
-    padding-bottom: 60px;
+    padding-top: 40px;
+    padding-bottom: 40px;
   }
 
   .ion-card-container {
     display: grid;
-    grid-template-rows: 80px auto;
+    grid-template-rows: auto auto;
     justify-items: center;
     align-items: center;
-    width: 140px;
-  }
-
-  .ion-card {
-    background-color: white;
-    padding: 1rem;
-    display: grid;
-    box-shadow: 0 1px 4px rgba(12, 12, 13, 0.1);
-    border-radius: 4px;
+    width: 170px;
   }
 
   .ion-card-description {
-    padding: 0.75rem;
+    /* padding: 0.75rem; */
     padding-bottom: 0;
     align-self: start;
   }
@@ -81,34 +70,28 @@
     <h2
       class="mzp-c-call-out-title mzp-has-zap-1"
       in:fly={{ duration: 800, y: 5 }}>
-      <Logo />
+      <img src="public/img/logo-wide.svg" alt="Mozilla Rally Logo" />
     </h2>
 
     <p
       class="mzp-c-call-out-desc"
-      style="color: var(--color-light-gray-90);
+      style="color: var(--color-marketing-gray-70);
 ">
-      A data contribution platform for people to support causes they believe in
+      Put your data to work and start building a better internet. But first:
     </p>
 
     <div in:fly={{ duration: 400, y: 10 }} class="ion-three-cards">
       <div class="ion-card-container">
-        <div class="ion-card">
-          <Graph />
-        </div>
-        <div class="ion-card-description">1. agree to our data collection</div>
+        <img src="public/img/welcome-01.png" alt='review our privacy policy' />
+        <div class="ion-card-description">Review and agree to our Privacy Notice</div>
       </div>
       <div class="ion-card-container">
-        <div class="ion-card">
-          <Profile />
-        </div>
-        <div class="ion-card-description">2. complete your profile</div>
+        <img src="public/img/welcome-02.png" alt='tell us about yourself' />
+        <div class="ion-card-description">Tell us about yourself, if you want to</div>
       </div>
       <div class="ion-card-container">
-        <div class="ion-card">
-          <Study />
-        </div>
-        <div class="ion-card-description">3. join your first study</div>
+        <img src="public/img/welcome-03.png" alt='join studies' />
+        <div class="ion-card-description">Join the study that's right for you</div>
       </div>
     </div>
 
@@ -119,10 +102,10 @@
       <Button size="xl" product on:click={() => dispatch('get-started')}>
         Get Started
       </Button>
-      <div class="how-it-works">
-        <a target="_blank" href="__BASE_SITE__/how-rally-works/">how it works
-          <ExternalLink /></a>
-      </div>
+    </div>
+    <div class="how-it-works">
+      <a target="_blank" href="__BASE_SITE__/how-rally-works/">Wait – how does it work again?
+        <ExternalLink /></a>
     </div>
   </div>
 </section>
