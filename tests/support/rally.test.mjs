3ef4d70c46748aaff9f8fe85c2da1fce0601bc7b
Alessio Placitelli <alessio.placitelli@gmail.com>
2021-02-02 10:24:47 +0100

    Fix running rally.js tests
    Even though mocha [supports ESM](https://mochajs.org/#nodejs-native-esm-support),
    there's a few changes required to make this work.
    This commit changes the test extension to `mjs` and changes the way
    dependencies are imported.

--- tests/support/rally.test.mjs
+++ tests/support/rally.test.mjs
@@ -2,9 +2,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-var assert = require('assert').strict;
+import { strict as assert } from 'assert';
 
-var Rally = require('../../support/rally');
+import Rally from '../../support/rally.js';
 
 describe('Rally', function () {
   beforeEach(function () {
