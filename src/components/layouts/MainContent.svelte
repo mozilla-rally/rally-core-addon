be19e5989bf8f0caba2f64cbbaff9d32761ee160
hamilton <hamilton.ulmer@gmail.com>
2021-02-04 07:40:57 -0800

    re-implemented study card bg

--- src/components/layouts/MainContent.svelte
+++ src/components/layouts/MainContent.svelte
@@ -1,15 +1,21 @@
+<script> /* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+export let pad = true;
+</script>
 <style>
 div {
     display: grid;
     grid-template-columns: var(--content-width);
     background-size: 100% 100%;
     background-attachment: fixed;
-    margin: 2rem 2.5rem;
+    margin: var(--pad, 0);
     margin-right: 0;
     box-sizing: border-box;
 }
 </style>
 
-<div>
+<div style="--pad: {pad ? "2rem 2.5rem" : "0"};">
     <slot />
 </div>
--- src/routes/current-studies/Background.svelte
+++ src/routes/current-studies/Background.svelte
@@ -6,23 +6,37 @@ import { tweened } from 'svelte/motion';
 import { cubicOut as easing } from 'svelte/easing';
 
 let t = tweened(1, { duration: 800});
-let p = tweened(1, {duration: 900, easing})
+let p = tweened(1, {duration: 1100, easing});
+let m = tweened(0, {duration: 500, easing});
 
 $: t.set(0);
 $: p.set(0);
+
+let innerHeight = 0;
+let scrollY = 0;
+
+$: documentHeight = document.body.height || 1000;
+$: totalDist = documentHeight - innerHeight;
+$: percentScrolled = scrollY / totalDist;
+$: m.set(percentScrolled);
+
 </script>
 
 <style>
 div {
+    padding: 2rem 2.5rem;
     background-image: 
         linear-gradient(to left, rgba(248, 248, 250, var(--o, 1)), rgba(248, 248, 250, var(--o, 1))), 
-        url('img/study-texture.png');
+        url('img/study-card-bg-top.png'), url('img/study-card-bg-bottom.png');
     background-size: auto 100%;
     background-repeat: no-repeat;
-    background-position: calc(15rem + var(--content-width) * .85) var(--p1, 0);
+    background-position: left top, calc(15rem + var(--content-width) * .85) var(--p1, 0), calc(15rem + var(--content-width) * .85) var(--p2, 0);
     background-attachment: fixed;
 }
 </style>
-<div style="--o: {$t}; --p1: {$p * 20}px; --p2: {$p * 10}px;">
+
+<svelte:window bind:scrollY bind:innerHeight />
+
+<div style="--o: {$t}; --p1: {$p * 35 + $m * 3}px; --p2: {$p * 20  - $m * 3}px;">
     <slot />
 </div>
