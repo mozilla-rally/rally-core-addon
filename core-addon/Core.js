b58b589dc55d94aeaf44df723fb7e6e133d10f1d
Hamilton Ulmer <hamilton.ulmer@gmail.com>
2021-02-09 16:55:54 -0800

    redirect user to "sorry to see you go" page after uninstall (#362)
    * adds redirect URL location for user uninstall
    
    * removes unnecessary whitespace
    
    * CHANGELOG.md

--- core-addon/Core.js
+++ core-addon/Core.js
@@ -43,6 +43,8 @@ module.exports = class Core {
   }
 
   initialize() {
+    // set the URL to redirect when a user uninstalls Rally
+    browser.runtime.setUninstallURL("__BASE_SITE__/leaving-rally");
     // Whenever the addon icon is clicked, open the control page.
     browser.browserAction.onClicked.addListener(this._openControlPanel);
     // After installing the addon, make sure to show the control page.
