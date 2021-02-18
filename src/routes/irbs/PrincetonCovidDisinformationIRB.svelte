30b8aafbc4f854901bad2fee3eb889ef8d51e759
hamilton <hamilton.ulmer@gmail.com>
2021-02-17 19:06:12 -0800

    changes size of IRB consent title element

--- src/routes/irbs/PrincetonCovidDisinformationIRB.svelte
+++ src/routes/irbs/PrincetonCovidDisinformationIRB.svelte
@@ -13,7 +13,7 @@
 }
 
 h1 {
-    font-size: 32px;
+    font-size: 24px;
     font-family: "Zilla Slab", -apple-system, BlinkMacSystemFont, "Segoe UI",
       Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji",
       "Segoe UI Symbol";
