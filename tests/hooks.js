631fbcc79439cb33f4853fd5a3189ed84ddb79f9
Sarthak Dayal <dayalsarthak5@gmail.com>
2021-02-18 20:27:36 -0600

    Update require to import
    Update require to import keyword to better align with ES6 standards.

--- tests/hooks.js
+++ tests/hooks.js
@@ -2,14 +2,14 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-const chrome = require('sinon-chrome/extensions');
+import { chrome } from 'sinon-chrome/extensions';
 // We need to provide the `browser.runtime.id` for sinon-chrome to
 // be happy and play nice with webextension-polyfill. See this issue:
 // https://github.com/mozilla/webextension-polyfill/issues/218
 chrome.runtime.id = "testid";
 global.chrome = chrome;
 
-var browser = require("webextension-polyfill");
+import { browser } from "webextension-polyfill";
 
 exports.mochaHooks = {
   beforeAll() {
