2109c683f939fc834a6c497486f0da42422963c9
Sarthak-Dayal <63827830+Sarthak-Dayal@users.noreply.github.com>
2021-02-19 15:30:29 -0600

    Fix import and change file name to .mjs

--- core-addon/background.js
+++ core-addon/background.js
@@ -1,13 +1,17 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
+/**
+ * /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * @format
+ */
 
-import { Core } from "./Core.js"
+import "./Core.js";
 import "webextension-polyfill";
 
 const core = new Core({
-    availableStudiesURI: __STUDIES_LIST__,
-    disableRemoteSettings: __DISABLE_REMOTE_SETTINGS__,
-    website: __WEBSITE_URL__,
+  availableStudiesURI: __STUDIES_LIST__,
+  disableRemoteSettings: __DISABLE_REMOTE_SETTINGS__,
+  website: __WEBSITE_URL__,
 });
 core.initialize();
