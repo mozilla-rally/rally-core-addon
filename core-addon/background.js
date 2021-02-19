d4e817c9d4347511a81cb5e83f1a5e69123461cb
Sarthak Dayal <dayalsarthak5@gmail.com>
2021-02-18 20:02:05 -0600

    Update require to import
    Change require keyword to import to align with standard ES6 guidelines.

--- core-addon/background.js
+++ core-addon/background.js
@@ -2,8 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+import { Core } from "./Core.js"
 window.browser = require("webextension-polyfill");
-const Core = require("./Core.js");
 
 const core = new Core({
     availableStudiesURI: __STUDIES_LIST__,
