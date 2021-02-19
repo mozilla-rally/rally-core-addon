889936021f61caddf63ed271093782d68fa1549e
Sarthak Dayal <dayalsarthak5@gmail.com>
2021-02-18 20:13:44 -0600

    Update require to import
    Change require keyword to import keyword to better align with ES6 standards.

--- core-addon/content-script.js
+++ core-addon/content-script.js
@@ -2,7 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-window.browser = require("webextension-polyfill");
+import * as browser from "webextension-polyfill";
+window.browser = browser;
 
 /**
  * Send an event to the web page.
