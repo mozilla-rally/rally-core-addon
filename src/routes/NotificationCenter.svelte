41cfb5566fecc0c6f5d332b36f51ce36e33e5b6b
hamilton <hamilton.ulmer@gmail.com>
2021-03-01 20:24:44 -0800

    addresses code nit

--- src/routes/NotificationCenter.svelte
+++ src/routes/NotificationCenter.svelte
@@ -1,17 +1,20 @@
 <script>
-	import { notification } from '../notification-store';
-    import SuccessfullyJoinedStudyNotification from "./current-studies/SuccessfullyJoinedStudyNotification.svelte";
-    import SuccessfullyLeftStudyNotification from "./current-studies/SuccessfullyLeftStudyNotification.svelte";
-    import SuccessfullyUpdatedProfileNotification from "./demographics/SuccessfullyUpdatedProfileNotification.svelte";
-    
-    export let sidebarOffset = false;
-    
-    const messages = {
-        SUCCESSFULLY_JOINED_STUDY: SuccessfullyJoinedStudyNotification,
-        SUCCESSFULLY_LEFT_STUDY: SuccessfullyLeftStudyNotification,
-        SUCCESSFULLY_UPDATED_PROFILE: SuccessfullyUpdatedProfileNotification
-    }
-  </script>
+/* This Source Code Form is subject to the terms of the Mozilla Public
+* License, v. 2.0. If a copy of the MPL was not distributed with this
+* file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+import { notification } from '../notification-store';
+import SuccessfullyJoinedStudyNotification from "./current-studies/SuccessfullyJoinedStudyNotification.svelte";
+import SuccessfullyLeftStudyNotification from "./current-studies/SuccessfullyLeftStudyNotification.svelte";
+import SuccessfullyUpdatedProfileNotification from "./demographics/SuccessfullyUpdatedProfileNotification.svelte";
+
+export let sidebarOffset = false;
+
+const messages = {
+    SUCCESSFULLY_JOINED_STUDY: SuccessfullyJoinedStudyNotification,
+    SUCCESSFULLY_LEFT_STUDY: SuccessfullyLeftStudyNotification,
+    SUCCESSFULLY_UPDATED_PROFILE: SuccessfullyUpdatedProfileNotification
+}
+</script>
   
 {#key $notification.id}
     {#if $notification.id}
