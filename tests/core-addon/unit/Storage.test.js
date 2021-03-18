2f476a70fc73f9d739add7e1df1d101c520f84b1
Alessio Placitelli <alessio.placitelli@gmail.com>
2021-03-18 18:11:18 +0100

    Fix unit tests
    Fix tests to use ES6 imports
    
    Note that this leaves the hooks file to be interpreted
    as a Common JS file, due to troubles importing
    sinon-chrome/extensions.

--- tests/core-addon/unit/Storage.test.js
+++ tests/core-addon/unit/Storage.test.js
@@ -2,9 +2,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-const assert = require('assert').strict;
+import { strict as assert } from 'assert';
 
-var Storage = require('../../../core-addon/Storage');
+import Storage from '../../../core-addon/Storage.js';
 
 describe('Storage', function () {
   beforeEach(function () {
