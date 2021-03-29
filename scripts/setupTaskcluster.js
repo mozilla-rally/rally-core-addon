d37be7cdc1f3669b736e90a13541d2d6e28eab8f
Alessio Placitelli <alessio.placitelli@gmail.com>
2021-03-29 09:37:11 +0200

    Ensure the right dependencies are available on TaskCluster
    Unfortunately this "hack" is required because the add-ons
    pipeline does not enable consumers to customize the installed
    packages.

---
+++ scripts/setupTaskcluster.js
@@ -0,0 +1,22 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import * as exec from "child_process";
+import { promisify } from "util";
+
+// Define an async/await version of "exec".
+const execAsync = promisify(exec.exec);
+
+// If this is not running on TaskCluster, exit
+// without an error code.
+if ('TASK_ID' in process.env) {
+  console.log("Running in TaskCluster. Running set-up.");
+
+  // The TaskCluster instance on the Mozilla add-ons pipeline
+  // does not allow image customization, so python3 venv cannot
+  // be installed. That package is required for Glean to build.
+  // We make sure to have all the dependencies in place when on
+  // TaskCluster by running the commands manually.
+  execAsync("sudo apt-get -y install python3-venv");
+}
