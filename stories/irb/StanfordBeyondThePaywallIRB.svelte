18450f4c949e0a199c8c8a30c0711d28f92b1ce1
hamilton <hamilton.ulmer@gmail.com>
2021-06-01 10:00:14 -0700

    add the Stanford Beyond the Paywall IRB component; add story component

---
+++ stories/irb/StanfordBeyondThePaywallIRB.svelte
@@ -0,0 +1,14 @@
+<script>
+    /* This Source Code Form is subject to the terms of the Mozilla Public
+     * License, v. 2.0. If a copy of the MPL was not distributed with this
+     * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+    
+    import IRB from "../../src/routes/irbs/StanfordBeyondThePaywallIRB.svelte";
+    import IRBWindow from "../../src/routes/irbs/IRBWindow.svelte";
+    </script>
+    
+    <div style="--height: 700px;">
+    <IRBWindow>
+        <IRB />
+    </IRBWindow>
+    </div>
